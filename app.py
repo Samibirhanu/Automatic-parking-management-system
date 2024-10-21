7f6b37cae08425c53769f4ab6a490304e24eb4aa
samuel <84203477+Samibirhanu@users.noreply.github.com>
2024-10-21 12:52:37 +0300

    flask app file upload

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import os
 # webserver gateway interface
 app = Flask(__name__)
 
+BASE_PATH = os.getcwd()
+UPLOAD_PATH = os.path.join(BASE_PATH, './static/upload/')
 
-
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('layout.html')
+    if request.method == 'POST':
+        upload_file = request.files['image_name']
+        filename = upload_file.filename
+        path_save = os.path.join(UPLOAD_PATH, filename)
+        upload_file.save(path_save)
+
+        return render_template('index.html')
+
+
+    return render_template('index.html')
 
 if __name__ =="__main__":
     app.run(debug=True)
